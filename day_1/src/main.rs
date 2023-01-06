8ae55a3b2d074e5d947627566179a1b7bd517a2f
Jayden Dumouchel <jdumouch@ualberta.ca>
2023-01-06 07:46:38 -0700

    Day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    // Parse input into Vec
+    let mut global_max = 0u64;
+    let mut running_max = 0u64;
+    for line in include_str!("../input").lines() {
+        if line.is_empty() {
+            if running_max > global_max {
+                global_max = running_max;
+            }
+
+            running_max = 0;
+            continue;
+        }
+
+
+        running_max += line.parse::<u64>().unwrap();
+    }
+
+    println!("{}", global_max);
+
 }
